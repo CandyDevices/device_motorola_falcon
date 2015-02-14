ce1fc042da501486785501237fe4a035e5fd2c3a
Dennis S Lebhardt jr. <dennislebhardt@yahoo.com>
2015-02-14 15:37:51 -0600

    SUGARRUSH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,10 @@ TARGET_OTA_ASSERT_DEVICE := xt1031,xt1032,xt1033,xt1034,falcon_umts,falcon_umtsd
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := falcon_defconfig
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-sm/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+SUGARRUSH=true
+KRAIT_TUNINGS=true
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
